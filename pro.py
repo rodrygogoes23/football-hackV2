ee4094046f011dd134b3fb7bebdda888f9f96289
PrinceVegeta447 <vegetakun447@gmail.com>
2025-02-03 22:47:58 +0530

    Update pro.py

--- pro.py
+++ pro.py
@@ -2,6 +2,16 @@ import os
 from pyrogram import Client, filters
 from pyrogram.types import Message
 from Mukund import Mukund
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/health')
+def health_check():
+    return "OK", 200
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000)
 
 storage = Mukund("Vegeta")
 db = storage.database("marvel")
